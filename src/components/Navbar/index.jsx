bfa6cb50519d55df01996035729c57166ac99b30
utkar-sh-ukla <utkarshshukla707@gmail.com>
2024-05-04 18:23:00 +0530

    [feat]: added Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1 className="navbar__logo">MARVEL</h1>
+      <input
+        type="search"
+        className="navbar__search"
+        placeholder="Search for Comics"
+      />
+    </nav>
+  );
+};
+
+export default Navbar;
